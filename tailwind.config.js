5657a514eb603ea8120a6f326e5e06c5773be710
Ricardo Gonçalves <ricardo@conceitho.com>
2026-03-17 18:58:01 -0300

    fix: correção tema escuro

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,13 @@ module.exports = {
       colors: {
         peau: {
           /* Usando rgba() nativo que funciona perfeitamente com as vírgulas do input.css */
-          light: 'rgba(var(--color-peau-light), <alpha-value>)',
-          rose: 'rgba(var(--color-peau-rose), <alpha-value>)',
-          rosedark: 'rgba(var(--color-peau-rosedark), <alpha-value>)',
-          dark: 'rgba(var(--color-peau-dark), <alpha-value>)',
-          gray: 'rgba(var(--color-peau-gray), <alpha-value>)',
-          card: 'rgba(var(--color-peau-card), <alpha-value>)',
-          inverse: 'rgba(var(--color-peau-inverse), <alpha-value>)',
+          light: 'rgb(var(--color-peau-light) / <alpha-value>)',
+          rose: 'rgb(var(--color-peau-rose) / <alpha-value>)',
+          rosedark: 'rgb(var(--color-peau-rosedark) / <alpha-value>)',
+          dark: 'rgb(var(--color-peau-dark) / <alpha-value>)',
+          gray: 'rgb(var(--color-peau-gray) / <alpha-value>)',
+          card: 'rgb(var(--color-peau-card) / <alpha-value>)',
+          inverse: 'rgb(var(--color-peau-inverse) / <alpha-value>)',
         },
         whatsapp: '#25D366',
         whatsappdark: '#1ea952'
